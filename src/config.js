f64674e7adbf780c4c533eebd60fa26cb6b6d6fc
Abdykali Azat <azat.abdykalli@gmail.com>
2024-06-06 08:29:29 +0500

    battak commit client

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://battak-kargo.kz:3001',
+    
+    nameCargo: 'Battak-Kargo'
+  };
+  
+  
+export default config;
+  
